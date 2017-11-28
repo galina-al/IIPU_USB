45d696f4a122f0958ab6bb580a64d51d088ef7de
Galina Al-Sheikhakhmad <galyunya123@gmail.com>
2017-11-28 21:00:07 +0300

    form

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Laba_4
+{
+    partial class Form1     
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.usbList = new System.Windows.Forms.DataGridView();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.spaceTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.usbList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usbList
+            // 
+            this.usbList.BackgroundColor = System.Drawing.Color.White;
+            this.usbList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usbList.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.usbList.Location = new System.Drawing.Point(12, 12);
+            this.usbList.Name = "usbList";
+            this.usbList.Size = new System.Drawing.Size(142, 150);
+            this.usbList.TabIndex = 0;
+            this.usbList.SelectionChanged += new System.EventHandler(this.ChangeSelect);
+            // 
+            // removeButton
+            // 
+            this.removeButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.removeButton.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.removeButton.Location = new System.Drawing.Point(323, 168);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(100, 34);
+            this.removeButton.TabIndex = 1;
+            this.removeButton.Text = "Извлечь";
+            this.removeButton.UseVisualStyleBackColor = false;
+            this.removeButton.Click += new System.EventHandler(this.OnclickButton);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 8000;
+            this.timer.Tick += new System.EventHandler(this.TickTimer);
+            // 
+            // spaceTextBox
+            // 
+            this.spaceTextBox.BackColor = System.Drawing.Color.White;
+            this.spaceTextBox.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.spaceTextBox.Location = new System.Drawing.Point(160, 12);
+            this.spaceTextBox.Multiline = true;
+            this.spaceTextBox.Name = "spaceTextBox";
+            this.spaceTextBox.Size = new System.Drawing.Size(263, 150);
+            this.spaceTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(437, 208);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.spaceTextBox);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.usbList);
+            this.Name = "Form1";
+            this.Text = "USB Manager";
+            this.Load += new System.EventHandler(this.LoadForm);
+            ((System.ComponentModel.ISupportInitialize)(this.usbList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView usbList;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox spaceTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_4
+{
+    //Class of form, where we have all important methods for GUI
+    public partial class Form1 : Form
+    {
+        //Constant for rewriting system method
+        private const int WM_DEVICECHANGE = 0X219;
+        private static readonly Manager _manager = new Manager();
+        private List<Usb> _deviceList;
+        //Our selecting table
+        private readonly DataTable _table = new DataTable();
+
+        //We follow for system messages
+        protected override void WndProc(ref Message m)
+        {
+            //Redirect messages to our program
+            base.WndProc(ref m);
+            //If config of our devices has changed
+            if (m.Msg == WM_DEVICECHANGE)
+            {
+                //Reload our data
+                ReloadForm();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //When we first load our form
+        private void LoadForm(object sender, EventArgs e)
+        {
+            _deviceList = new List<Usb>();
+            //We should make a template for our program
+            _table.Columns.Add("Название", typeof(string));
+            //Get all our devices
+            ReloadForm();
+            usbList.DataSource = _table;
+            removeButton.Enabled = false;
+            timer.Enabled = true;
+        }
+        //Method for reloading a program
+        private void ReloadForm()
+        {
+            int currentPosition = 0;
+            //Checking if our selected row is chosen
+            if (usbList.CurrentRow != null)
+            {
+                currentPosition = usbList.CurrentRow.Index;
+            }
+            //Delete all past data
+            _table.Clear();
+            _deviceList = _manager.DeviseListCreate();
+            foreach(Usb device in _deviceList)
+            {
+                _table.Rows.Add(device.DeviceName);
+            }
+            //If there are no index of bounds
+            if (usbList.RowCount - 1 > currentPosition)
+            {
+                //Then we select this row
+                usbList.Rows[currentPosition].Selected = true;
+            }
+            label1.Text = "";
+        }
+
+        //Timer for live reload every 5 seconds
+        private void TickTimer(object sender, EventArgs e)
+        {
+            ReloadForm();
+        }
+
+        //Event for selecting some row in list
+        private void ChangeSelect(object sender, EventArgs e)
+        {
+            //If row is exists
+            if (usbList.CurrentRow != null)
+            {
+                //If there are no index of bounds
+                if (usbList.CurrentRow.Index >= 0 && usbList.CurrentRow.Index < _deviceList.Count)
+                {
+                    //We can eject only USB, not MTP
+                    removeButton.Enabled = !_deviceList[usbList.CurrentRow.Index].IsMtpDevice;
+                    //The same with output
+                    if (!_deviceList[usbList.CurrentRow.Index].IsMtpDevice)
+                    {
+                        spaceTextBox.Text = "Свободно памяти: " + _deviceList[usbList.CurrentRow.Index].FreeSpace + "\r\n" +
+                                        "Занято памяти: " + _deviceList[usbList.CurrentRow.Index].UsedSpace + "\r\n" +
+                                        "Общая память: " + _deviceList[usbList.CurrentRow.Index].TotalSpace;
+                    }
+                }
+                else
+                {
+                    //In other situations just block everything
+                    removeButton.Enabled = false;
+                    spaceTextBox.Text = "";
+                }
+            }
+        }
+
+        //Event for clicking deleting button
+        private void OnclickButton(object sender, EventArgs e)
+        {
+            //If we choose what device we want to eject
+            if (usbList.CurrentRow != null)
+            {
+                //Then we should invoke a method Eject_Device()
+                bool isEjected = _deviceList[usbList.CurrentRow.Index].EjectDevice();
+                if (isEjected == false)
+                {
+                    label1.Text = "Устройство занято.";
+                }
+                else
+                {
+                    spaceTextBox.Text = "";
+                }
+            }
+        }
+    }
+}
