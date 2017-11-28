3b14d965ab7e08039b08c1eb94910d3d7a880734
Galina Al-Sheikhakhmad <galyunya123@gmail.com>
2017-11-28 21:02:36 +0300

    all

---
+++ Usb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsbEject;
+
+namespace Laba_4
+{
+    class Usb
+    {
+        public string DeviceName { get; set; }
+        public string FreeSpace { get; set; }
+        public string UsedSpace { get; set; }
+        public string TotalSpace { get; set; }
+        public bool IsMtpDevice { get; set; }
+
+        public Usb(string name, string freeSize, string usedSize, string totalSize, bool check)
+        {
+            DeviceName = name;
+            FreeSpace = freeSize;
+            UsedSpace = usedSize;
+            TotalSpace = totalSize;
+            IsMtpDevice = check;
+        }
+        public bool EjectDevice()
+        {
+            var tempName = this.DeviceName.Remove(2);
+            var ejectedDevice = new VolumeDeviceClass().SingleOrDefault(v => v.LogicalDrive == this.DeviceName.Remove(2));
+            ejectedDevice.Eject(false);
+            ejectedDevice = new VolumeDeviceClass().SingleOrDefault(v => v.LogicalDrive == tempName);
+            if (ejectedDevice == null)
+                return true;
+            else
+                return false;
+        }
+    }
+}
